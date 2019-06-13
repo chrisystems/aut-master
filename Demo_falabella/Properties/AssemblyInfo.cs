1359250af3aa2ffb0bdc547b9a974a25d009440a
chrisystems <51771601+chrisystems@users.noreply.github.com>
2019-06-12 22:28:04 -0500

    Add files via upload

---
+++ Demo_falabella/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Demo_falabella")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Demo_falabella")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("778b2bd2-fd61-4d91-91c5-1d334a1776e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Demo_falabella/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;           
+using OpenQA.Selenium.Firefox;   
+using OpenQA.Selenium.Chrome;    
+using OpenQA.Selenium.IE;
+
+namespace SeleniumBingTests
+{
+  /// <summary>
+  /// Summary description for MySeleniumTests
+  /// </summary>
+  [TestClass]
+  public class MySeleniumTests
+  {
+    private TestContext testContextInstance;
+    private IWebDriver driver;
+    private string appURL;
+
+    public MySeleniumTests()
+    {
+    }
+
+    [TestMethod]
+    [TestCategory("chrome")]
+    public void DemoTest()
+    {
+      driver.Navigate().GoToUrl(appURL + "/");
+      driver.FindElement(By.XPath("//*[contains(text(), 'Build GitHub projects using Azure Pipelines')]")).Click();
+      }
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    [TestInitialize()]
+    public void SetupTest()
+    {
+      appURL = "http://localhost:8090";
+
+      string browser = "Firefox";
+      switch(browser)
+      {
+        case "Chrome":
+          driver = new ChromeDriver();
+          break;
+        case "Firefox":
+          driver = new FirefoxDriver();
+          break;
+        case "IE":
+          driver = new InternetExplorerDriver();
+          break;
+        default:
+          driver = new ChromeDriver();
+          break;
+      }
+
+    }
+
+    [TestCleanup()]
+    public void MyTestCleanup()
+    {
+      driver.Quit();
+    }
+  }
+}
